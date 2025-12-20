b9f0efb82608bb6b8cd21fa58583e3fbcfc744a3
unknown <morrshidkhalid2@gmail.com>
2025-12-20 18:08:36 +0300

    20/12

--- esh3arsql.sql
+++ esh3arsql.sql
@@ -8,6 +8,24 @@ SELECT * FROM [Esh3arTechDb_v101].[dbo].[EtUserPlans]
 
 SELECT * FROM [Esh3arTechDb_v101].[dbo].[AbpUsers]
 
+SELECT 
+	CASE
+		WHEN
+			ISNULL(EtUserPlans.DailyPrice, 0) = 0 AND
+			ISNULL(EtUserPlans.WeeklyPrice, 0) = 0 AND
+			ISNULL(EtUserPlans.MonthlayPrice, 0) = 0 AND
+			ISNULL(EtUserPlans.AnnualPrice, 0) = 0 
+		THEN 1
+		ELSE 0
+	END AS IsFree
+FROM EtUserPlans
+
+--UPDATE  [Esh3arTechDb_v101].[dbo].[AbpUsers] SET PlanId = '5F82439A-8516-D297-8ADE-3A1DC7246E18' Where Id = '51076BC4-E24A-1B43-A9A3-3A1DC2F8E982'
+
+--UPDATE  [Esh3arTechDb_v101].[dbo].[AbpUsers] SET PlanId = null
+
+--UPDATE  [Esh3arTechDb_v101].[dbo].[EtUserPlans] SET IsDeleted = 0
+
 --TRUNCATE TABLE [Esh3arTechDb_v101].[dbo].[AbpFeatureGroups]
 
 --TRUNCATE TABLE [Esh3arTechDb_v101].[dbo].[AbpFeatures]
@@ -39,3 +57,84 @@ UPDATE EtUserPlans SET AnnualPrice = 60 Where Id = 'D55FB645-A065-44E8-964D-8778
 
 SELECT up.Name, up.DisplayName, sup.DisplayName as ExpireName from EtUserPlans up
 left join EtUserPlans sup on up.ExpiringPlanId = sup.Id
+
+
+-- Subscriptions sections
+SELECT UserName, EtUserPlans.Name, BillingInterval, Price, StartDate, EndDate, NextBill, EtUserPlans.CreationTime
+FROM EtSubscriptions
+Join AbpUsers on EtSubscriptions.UserId = AbpUsers.Id
+Join EtUserPlans on EtSubscriptions.PlanId = EtUserPlans.Id
+order by CreationTime DESC
+
+SELECT * FROM EtSubscriptions Where ID = 'E59F998D-6358-DEED-40EE-3A1E194CBAC8'
+
+SELECT * FROM EtSubscriptionRenewalHistory WHERE SubscriptionId = 'E59F998D-6358-DEED-40EE-3A1E194CBAC8'
+
+SELECT UserName, EtUserPlans.Name, EtSubscriptionRenewalHistory.* 
+FROM EtSubscriptions
+Join AbpUsers on EtSubscriptions.UserId = AbpUsers.Id
+Join EtUserPlans on EtSubscriptions.PlanId = EtUserPlans.Id
+Join EtSubscriptionRenewalHistory on EtSubscriptions.Id = EtSubscriptionRenewalHistory.SubscriptionId
+Where EtSubscriptionRenewalHistory.SubscriptionId = 'E59F998D-6358-DEED-40EE-3A1E194CBAC8'
+
+SELECT * FROM AbpUsers
+
+DELETE FROM EtSubscriptions
+
+TRUNCATE TABLE EtSubscriptionRenewalHistory
+
+UPDATE AbpUsers SET PlanId = null
+
+UPDATE EtSubscriptions SET StartDate = DATEADD(DAY, -1, StartDate)
+
+UPDATE EtSubscriptions SET EndDate = DATEADD(DAY, -1, EndDate)
+
+UPDATE EtSubscriptions SET NextBill = DATEADD(DAY, -1, NextBill)
+
+UPDATE EtSubscriptionRenewalHistory SET RenewalDate = DATEADD(DAY, -1, RenewalDate)
+
+UPDATE EtSubscriptionRenewalHistory SET PeriodStartDate = DATEADD(DAY, -1, PeriodStartDate)
+
+UPDATE EtSubscriptionRenewalHistory SET PeriodEndDate = DATEADD(DAY, -1, PeriodEndDate)
+
+SELECT * FROM EtUserPlans
+
+UPDATE EtSubscriptions SET IsActive = 1
+
+-- Messages sections
+-- Pending = 0,    // Created, not yet sent
+-- Sent = 1,       // Enqueued for delivery
+-- Delivered = 2,  // Received by recipient device
+-- Read = 3,       // Opened by recipient
+-- Queued = 4,      // Waiting for action
+-- Failed = 5      // Delivery failed after retries
+
+SELECT * FROM AbpUsers
+
+SELECT 
+    RecipientPhoneNumber,
+    Subject,
+    MessageContent,
+    CASE 
+        WHEN Status = 0 THEN 'Pending'
+        WHEN Status = 1 THEN 'Sent'
+        WHEN Status = 2 THEN 'Delivered'
+        WHEN Status = 3 THEN 'Read'
+        WHEN Status = 4 THEN 'Queued'
+        WHEN Status = 5 THEN 'Failed'
+        ELSE 'Unknown'
+    END AS StatusDescription,
+	CreatorId
+FROM EtMessages;
+
+-- Mobile sections
+SELECT * FROM EtMobileUsers
+SELECT * FROM AbpUsers
+
+
+INSERT INTO EtMobileUsers (Id, MobileNumber, Status, IsStatic, CreationTime, IsDeleted)
+VALUES 
+(NEWID(), '967775265496', 1, 0, GETDATE(), 0),
+(NEWID(), '967775265497', 1, 0, GETDATE(), 0),
+(NEWID(), '967775265498', 1, 0, GETDATE(), 0),
+(NEWID(), '967775265499', 1, 0, GETDATE(), 0)
